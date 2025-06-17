5051f0b54ae7917422823b103b262c6439362708
SantanuAdhikary <santanuadhikary13@gmail.com>
2025-06-17 16:25:21 +0530

    array traverse project

---
+++ display_products/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+
+         <h1 className='logo'>Display <span>Products</span>  </h1>
+         <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact us</li>
+         </ul>
+    </nav>
+  )
+}
+
+export default Navbar
